efa4ecf1d033e8ff57af2495e24809a3d21e2912
Justin Miller <justin.miller@cnc0023.local>
2016-03-16 13:49:17 +0000

    added staticfiles config, adjusted app.js

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// static mappings are now configured in .ebsextensions/staticfiles.config --JM
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
